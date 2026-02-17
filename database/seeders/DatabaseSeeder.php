aee5dce98b9abe2859c358295c28e51912148f3f
Denis <5945069@alu.murciaeduca.es>
2026-02-17 09:47:45 +0100

    Añadiendo credenciales del admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,11 +21,12 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        /*User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => config('app.admin.email'),
+            'password' => config('app.admin.password'),
         ]);
-        */
+
 
         Model::unguard();
         Schema::disableForeignKeyConstraints();
