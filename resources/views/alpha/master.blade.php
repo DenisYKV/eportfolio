1f79f33da606531a858128ac23cb25278426fe50
Denis <5945069@alu.murciaeduca.es>
2026-02-18 08:38:29 +0100

    mostrando mensajes flash de sesion

--- resources/views/alpha/master.blade.php
+++ resources/views/alpha/master.blade.php
@@ -11,13 +11,16 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="{{ asset('/alpha/assets/css/main.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('css/flash-messages.css') }}">
 </head>
 
 <body class="landing is-preload">
+    <x-flash-messages />
     <div id="page-wrapper">
 
         <!-- Header -->
-       @include('alpha.partials.header')
+        @include('alpha.partials.header')
 
         <!-- Banner -->
         @include('alpha.partials.banner')
@@ -40,6 +43,15 @@
     <script src="{{ asset('/alpha/assets/js/breakpoints.min.js') }}"></script>
     <script src="{{ asset('/alpha/assets/js/util.js') }}"></script>
     <script src="{{ asset('/alpha/assets/js/main.js') }}"></script>
+    <script>
+        document.querySelectorAll('[data-flash]').forEach(el => {
+            setTimeout(() => {
+                el.style.transition = 'opacity 0.4s'
+                el.style.opacity = '0'
+                setTimeout(() => el.remove(), 400)
+            }, 4000) // ms antes de desaparecer
+        })
+    </script>
 
 </body>
 
